4d5452c33f0264d51befc26d70e37a3f6a625bef
Emil Milo Thyregod Geertsen <emtg71820@edu.ucl.dk>
2025-05-01 10:47:30 +0200

    1

--- Klsp/Program.cs
+++ Klsp/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Klsp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Klsp
 {
-    internal class Program
+    public abstract class Kunde
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public string MiddleNames { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Kunde(string name, string middleNames, string lastName, int phoneNumber,string email)
         {
-            Console.WriteLine("Hello, World!");
+            Name = name;
+            MiddleNames = middleNames;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+
+    public class Køber : Kunde
+    {
+        public Køber(string name, string middleNames, string lastName, int phoneNumber, string email):base(name,middleNames,lastName,phoneNumber,email)
+        {
+
+        }
+    }
+
+    public class Sælger : Kunde
+    {
+        public Sælger(string name, string middleNames, string lastName, int phoneNumber, string email):base(name,middleNames,lastName,phoneNumber,email)
+        {
+
         }
     }
 }
